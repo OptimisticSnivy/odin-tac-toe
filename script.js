2211763f55091dd7576b8acec8e479e64c6131a4
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-16 19:46:15 +0530

    Basic objects done!

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function gameBoard(arr) {
+	this.arr = arr;
+	function showBoard() {
+		console.log(arr[0]);
+	}
+	function setMark(i,j) { arr[i][j] = "O"}
+	function setCross(i,j) { arr[i][j] = "X"}
+	function isColWin() {
+		return arr[0] == ["O","O","O"];
+	}
+	return { arr, showBoard, setMark, setCross, isColWin };
+}
 
+
+let arr = [[],[],[]];
+let game = gameBoard(arr);
+game.setMark(0,0);
+game.setMark(0,1);
+game.setMark(0,2);
+game.showBoard();
+console.log(game.isColWin());
